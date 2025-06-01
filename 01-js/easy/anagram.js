e3e734afc80029f01fff81a194e6614c0bfa814a
Bharath200415 <bharath200415@gmail.com>
2025-06-01 13:24:31 +0530

    solved anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,18 @@
 */
 
 function isAnagram(str1, str2) {
+  // Normalize the strings: remove spaces, convert to lowercase, and sort the characters
+  function normalize(str){
+    return str
+      .replace(/\s+/g, '')  // Remove all whitespace
+      .toLowerCase()        // Convert to lowercase
+      .split('')            // Split into characters
+      .sort()               // Sort characters
+      .join('');            // Join back into a string
+
+  }
+  return normalize(str1) == normalize(str2);
+
 
 }
-
 module.exports = isAnagram;
