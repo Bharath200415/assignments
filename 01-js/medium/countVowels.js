469287a8c499404fd285d0b28fc97de8793f0943
Bharath200415 <bharath200415@gmail.com>
2025-06-04 10:13:47 +0530

    countVowel Solved

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,6 +6,17 @@
 */
 
 function countVowels(str) {
+  const vowels = "aeiou";
+  let count = 0;
+
+  for (let char of str.toLowerCase()){
+    if (vowels.includes(char)){
+      count++;
+    }
+  }
+
+  return count;
+
     // Your code here
 }
 
