97f406323e91538d56098511ffee2ddf7f3f3e1e
Bharath200415 <bharath200415@gmail.com>
2025-06-04 09:03:25 +0530

    solved expenditure analysis question

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -12,9 +12,35 @@
 	}
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
+let arr=[];
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const categoryMap = {};
+
+  for (let i=0;i<transactions.length;i++){
+    const transaction = transactions[i]
+    const category = transaction.category;
+    const price = transaction.price;
+
+    if (categoryMap[category]){
+      categoryMap[category] += price;
+    } else {
+      categoryMap[category] = price;
+    }
+  }
+
+  // Convert the map into the desired array of objects
+  const result = [];
+  for (let category in categoryMap) {
+  
+    result.push({
+      category: category,
+      totalSpent: categoryMap[category],
+    });
+  }
+
+  return result;
 }
+
 
 module.exports = calculateTotalSpentByCategory;
