7509c6336b8cb61e7e66cb86ee83b8a0e697745b
Bharath200415 <bharath200415@gmail.com>
2025-07-19 19:24:55 +0530

    updates

--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,14 +1,51 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, User, Course } = require("../db");
+const {JWT_SECRET} = require("../config");
 const router = Router();
+const jwt = require("jsonwebtoken");
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    await Admin.create({
+        username:username,
+        password:password
+    })
+    res.json({
+        msg:"user created successfully!"
+    })
+
+
 });
 
-router.post('/signin', (req, res) => {
+router.post('/signin',  async (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+    const user = await Admin.findOne({
+        username,
+        password
+    })
+    if (user){
+        const token = jwt.sign({
+            username,
+            password
+
+        },JWT_SECRET)
+        res.json({
+            token
+        })
+    }else{
+        res.status(411).json({
+            msg:"incorrect username or pass"
+
+        })
+    }
+
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
